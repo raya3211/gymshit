9d92f618a6dc728d30a0d451cb483c21077db506
raya <riqriq50@gmail.com>
2023-01-05 16:31:20 +0700

    Add files via upload

--- java.js
+++ java.js
@@ -2,7 +2,11 @@
 let [seconds, minutes, hours] = [0,0,0];
 let displayTime = document.getElementById("displayTime");
 var round = 0;
+var rest = 0;
 let timer = null;
+let roundTime = document.getElementById("roundTime");
+let restTime = document.getElementById("restTime");
+
 
 function stopwatch (){
     seconds++;
@@ -29,8 +33,7 @@ function watchStart(){
         clearInterval(timer);
     }
     timer = setInterval(stopwatch,1000);
-    round += 1;
-    document.getElementById("round").innerHTML = round;
+
 }
 
 function watchStop (){
@@ -41,4 +44,20 @@ function watchReset (){
     clearInterval (timer);
     [seconds, minutes, hours] = [0,0,0];
     displayTime.innerHTML = "00:00:00";
+    round += 1;
+    roundTime.innerHTML = round;
+    if(round > 4){
+    rest += 1;
+    restTime.innerHTML = rest;
+}
+}
+
+function ResetAll (){
+    clearInterval (timer);
+    [seconds, minutes, hours] = [0,0,0];
+    displayTime.innerHTML = "00:00:00";
+    round = 0;
+    roundTime.innerHTML = round;
+    rest = 0;
+    restTime.innerHTML = rest;
 }
